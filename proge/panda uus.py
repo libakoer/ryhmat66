93115269720d11d6a4950fc2cec546c29ec92da3
libakoer <90179538+libakoer@users.noreply.github.com>
2021-11-14 17:50:30 +0200

    Delete panda uus.py

--- proge/panda uus.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-print(pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'], 
-              'Sue': ['Pretty good.', 'Bland.']},
-             index=['Product A', 'Product B']))
